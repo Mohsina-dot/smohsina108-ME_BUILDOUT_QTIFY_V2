0ee8dea786667580bc7f8d13fa50a59dae22bff6
Mohsina-dot <smohsina108@gmail.com>
2025-12-03 08:21:36 +0530

    Fix headphones image path for Vercel build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import headphones from "../../assets/headphones.png";
+import headphones from "../assets/headphones.png";
 
 function Hero() {
   return (
