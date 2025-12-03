1e1a17932e2470b9e9c63759769b33aa96d1a709
Mohsina-dot <smohsina108@gmail.com>
2025-12-03 08:37:23 +0530

    Fix case-sensitive asset imports for Vercel build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import headphones from "../assets/headphones.png";
+import headphones from "../assets/Headphones.png";
 
 function Hero() {
   return (
